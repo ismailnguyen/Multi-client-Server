12809f8d524e18580ac373c4668a4aab5fcbe1e1
Ismaïl <nguyen.ismail@gmail.com>
2016-06-12 17:36:18 +0200

    run local commands from client (ls, pwd, cd, rm)

--- client.c
+++ client.c
@@ -9,69 +9,52 @@
 #include <netinet/tcp.h>
 #include <arpa/inet.h>
 #include <errno.h>
+#include <fcntl.h>
+#include <signal.h>
+#include <sys/wait.h>
 
 #define BUFFER_SIZE 1024
 
-void communicate(FILE* file, int socket_dialog);
+void communicate(int, FILE*);
+void command(int, char*);
 
 int main (int argc, char *argv[]) {
 
 	struct sockaddr_in server;
-	char ip_address[16], file_type[200];
-	int socket_client, res, port;
-	FILE* file;
-	char ;
+	int socket_client, res;
 
 	socket_client = socket(AF_INET, SOCK_STREAM, 0);
 	if (socket_client == -1) {
 		printf("Error during TCP socket establishment\n");
 		exit (10);
 	}
+	
+	if (argc != 3) {
+		printf("Usage: client <IPaddress> <Port>\n");
+		exit (-1);
+	}
 
-	if ((argv[1] != NULL) && (argv[2] != NULL) && (argv[3] != NULL)) {
-		strcpy(ip_address, argv[1]);
-		port = atoi(argv[2]);
-		strcpy(file_type, argv[3]);
-	}
-	else {
-		printf("IP address:\n");
-		scanf("%s", ip_address);
-			
-		printf("Port:\n");
-		scanf("%d", &port);
-		
-		printf("Source:\n");
-		scanf("%s", file_type);
-	}
-	
-	
-	if (strcmp(file_type, "stdin") != 0) {
-		file = fopen(file_type, "r");
-	}
-	else {
-		file = stdin;
-	}
-	
-	
+	bzero(&server, sizeof(server));
 	server.sin_family = AF_INET;
-	server.sin_addr.s_addr = inet_addr(ip_address);
-	server.sin_port = htons(port);
+	server.sin_port = htons(atoi(argv[2]));
+	inet_pton(AF_INET, argv[1], &server.sin_addr);
 	
 	res = connect(socket_client, (struct sockaddr*) &server, sizeof(server));
 	if (res == -1){
 		printf("Error while connecting\n");
-		exit (70);
+		exit (-2);
 	}
 	
 	printf("Connection established\n");
+	printf("\> ");
 	
-	communicate(file, socket_client);
+	communicate(socket_client, stdin);
 	
 	printf("Closing socket : ");
 	res = close(socket_client);
 	if (res == -1) {
 		printf("Error while closing socket #%d\n", socket_client);
-		exit (90);
+		exit (-3);
 	}
 
 	printf("Socket #%d closed\n", socket_client);
@@ -80,17 +63,52 @@ int main (int argc, char *argv[]) {
 	return 0;
 }
 
-void communicate(FILE* file, int socket_dialog) {
+void communicate(int socket_dialog, FILE* file) {
 	
 	char reception[BUFFER_SIZE], emission[BUFFER_SIZE];
 	int nb_char;
 	
 	while ((fgets(emission, BUFFER_SIZE, file) != NULL) && (strcmp(emission, "STOP\n") != 0)) {
 		
-		write(socket_dialog, emission, strlen(emission) + 1);
+		command(socket_dialog, emission);
 		
 		nb_char = read(socket_dialog, reception, BUFFER_SIZE);
 		
 		fputs(reception, stdout);
+		
+		printf("\n> ");
+	}
+}
+
+void command(int socket_dialog, char* input) {
+
+	if (strcmp(input, "ls\n") == 0) {
+		execlp("/bin/ls", "ls", NULL);
+	}
+	else if (strcmp(input, "pwd\n") == 0) {
+		execlp("/bin/pwd", "pwd", NULL);
+	}
+	else if (strcmp(input, "cd\n") == 0) {
+		char path[200];
+		
+		printf("Path:\n");
+		scanf("%s", &path);
+		
+		chdir(path);
+		
+		execlp("/bin/ls", "ls", NULL);
+	}
+	else if (strcmp(input, "rm\n") == 0) {
+		char path[200];
+		
+		printf("Path:\n");
+		scanf("%s", &path);
+		
+		unlink(path);
+		
+		execlp("/bin/ls", "ls", NULL);
+	}
+	else {
+		write(socket_dialog, input, strlen(input) + 1);
 	}
 }
